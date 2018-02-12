4ac087f0f2789f92c3b401fb47149e9b4367b134
Leon Georgi <naytde@gmail.com>
2018-02-12 15:37:54 +0100

    Add songs to album class for lazy evaluation

--- app/src/main/java/de/leongeorgi/celestina/backend/model/Album.kt
+++ app/src/main/java/de/leongeorgi/celestina/backend/model/Album.kt
@@ -1,5 +1,9 @@
 package de.leongeorgi.celestina.backend.model
 
+import android.content.ContentUris
+import android.net.Uri
+import java.util.*
+
 /**
  * @author leon
  */
@@ -7,6 +11,23 @@ data class Album(
         val id: Long,
         val name: String,
         val artistId: Long,
-        val artistName: String
+        val artistName: String,
+        val songs: List<Song>
 ) {
+    val songCount by lazy { songs.size }
+
+    val releaseYear by lazy {
+        songs.map { it.releaseYear }.filter { it > 0 }.min() ?:
+                Calendar.getInstance().get(Calendar.YEAR)
+    }
+
+    val dateAdded by lazy { songs.map { it.dateAdded }.min() }
+    val dateLastAdded by lazy { songs.map { it.dateAdded }.max() }
+
+    val coverPath by lazy {
+        ContentUris.withAppendedId(
+                Uri.parse("content://media/external/audio/albumart"),
+                id
+        )
+    }
 }
